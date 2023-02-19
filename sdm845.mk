605b0222a7302b04bd3a54bdc82635bbd90f3dd2
Adithya R <gh0strider.2k18.reborn@gmail.com>
2023-02-19 13:49:13 +0530

    sdm845-common: Disable ART debug and optimize dexpreopt
    This is a squash of the following commits:
    1. Optimise dex flags for a faster boot
    
    * Used multiple threads and speed profile to hasten the first boot
    
    2. Rework dex flags again
    
    When I went through https://source.android.com/devices/tech/dalvik/configure my previous configs felt wrong, this one should be perfect (I hope).
    Even though there is a slight trade-off for boot time by using the speed profile, we do make up for it by using 8 threads.
    PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER attempts to compile all prebuilts fully optimised to speed level, thus the phone doesnt need to deal with it.
    I also do know that this might be a trade-off on size, but I do see the first boot time is more worthy to trade-off.
    The speed level mentioned here runs dex verification and compiles all AOT methods.
    
    3. Switch certain dexopt profiles to verify
    
    Prebuilt apps like Gmail/Google App, will be updated by google play, and there is no need spend time and space optimising what is going to be replaced.
    Switch to Google's recommendation of using verify for an OTA package.
    Applications still stay on speed.
    Instead, replace the install profile with speed-profile making apps perform better.
    
     * minimize java debug info
     * use everything filter for dexpreopt (AOSPA/android_vendor_pa@ab5061c)
     * disable debug configs in art
     * use non-debug dex2oat
    
    Co-authored-by: baalajimaestro <me@baalajimaestro.me>
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Ic0156ff8956a7c5f53820be6db438f6f6399d3c0

--- sdm845.mk
+++ sdm845.mk
@@ -443,6 +443,9 @@ PRODUCT_PACKAGES += \
 
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+USE_DEX2OAT_DEBUG := false
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
